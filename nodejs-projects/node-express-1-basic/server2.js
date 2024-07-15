ad5f4821fd5e39c2e373b9559bdd630bc546b98f
eswar <mu.eswar@gmail.com>
2024-07-15 16:10:43 -0500

    update

---
+++ nodejs-projects/node-express-1-basic/server2.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res)=> {res.send("Hellow")});
+
+app.listen(8000,()=>{console.log(`server is running on 8000 port`)});
---
+++ nodejs-projects/node-express-2-static-file/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/',(req, res)=> {
+    res.sendFile(path.join(__dirname,'public','index.html'))
+});
+app.get('/about',(req, res)=> {
+    res.sendFile(path.join(__dirname,'public','about.html'))
+});
+app.listen(8000,()=>{console.log(`server is running on 8000 port`)});
